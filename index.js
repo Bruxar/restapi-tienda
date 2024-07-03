a64c7cbf6556c2eb2d209f227a314f3b05558f5b
Bruxar <larancibia@utem.cl>
2024-07-03 19:09:20 -0400

    CORS añadido

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 
+//Importar Cors (Permitir que un cliente se conecte a otro servidor para intercambio de datos)
+const cors = require('cors');
+
+//Conectar a MongoDB
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1/restapis')
 
@@ -11,6 +15,9 @@ const app = express();
 //Habilitar body parser
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//Habilitar Cors
+app.use(cors());
 
 //Rutas app
 app.use('/', routes());
